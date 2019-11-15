c6efc9deb739003701cddc515690621d55b0f0af
sstarnovskii <kopernik.t.800@gmail.com>
2019-11-15 22:11:31 +0700

    Finding language text function added

--- main.py
+++ main.py
@@ -17,7 +17,19 @@ else:
 txt = input(lc.INPUT)
 
 
-# TODO: make function to distinguish russian and english. (Sergey)
+# Finding language of the text.
+count_ru = 0
+count_en = 0
+for i in range(0, len(txt)):
+    if 97 <= ord(txt[i]) <= 122 or 65 <= ord(txt[i]) <= 90:
+        count_en += 1
+    elif 1072 <= ord(txt[i]) <= 1103 or 1040 <= ord(txt[i]) <= 1071:
+        count_ru += 1
+if count_ru > count_en:
+    lang_ru = True
+else:
+    lang_en = True
+
 
 # TODO: make functions to count: letters, cyllables, words and sentences.
 
